d17a6993ba3d99757859ea6906f75dc17fa34ae6
TommyBaskerville <147432909+TommyBaskerville@users.noreply.github.com>
2024-02-20 17:34:56 -0600

    Impresion
    Design application for print

--- Listas/Listas/Form1.cs
+++ Listas/Listas/Form1.cs
@@ -2,6 +2,8 @@ namespace Listas
 {
     public partial class Form1 : Form
     {
+        private string valor;
+
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +11,7 @@ namespace Listas
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            eliminar();
         }
 
         private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
@@ -22,6 +24,47 @@ namespace Listas
                 valor += ": " + dataGridView1.Rows[i].Cells[3].Value.ToString();
                 valor = "/n";
 
+            }
+            guardarComo(valor);
+        }
+
+        public void guardarComo(String valor)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.WriteAllText(saveFileDialog1.FileName, valor);
+            }
+        }
+
+        public void abrir()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String valor = System.IO.File.ReadAllText(openFileDialog1.FileName);
+                String[] lineas = valor.Split('\n');
+                for(int i = 0; i < lineas.Length; i++)
+                {
+                    String[] datos = lineas[i].Split(':');
+                    dataGridView1.Rows.Add(datos);
+                }
+            }
+        }
+        
+        public void eliminar()
+        {
+            if(dataGridView1.Rows.Count > 0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.CurrentRow.Index);
             }
         }
     }
--- Listas/Listas/obj/Debug/net8.0-windows/Listas.AssemblyInfo.cs
+++ Listas/Listas/obj/Debug/net8.0-windows/Listas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Listas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a2aea54b73390d92c118142397b0ab739179b35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d4bb0590d4adb8c7238f3bce65620916fb09f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Listas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Listas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
